d4789980ef5bb8c232f8b131a5c3ff4c7b16b2be
JaimeMAnayaRojas <jmanaya30@gmail.com>
2020-11-25 15:18:54 +0100

    Text corrections

--- R/Introduction Notebook.r
+++ R/Introduction Notebook.r
@@ -32,7 +32,6 @@ levels(animals)[3] <- "mammals"
 levels(animals)
 animals
 
-# Do this part in R directly
 # install.packages(c("coda","mvtnorm","devtools","loo","dagitty", 'rstan', "brms", "readxl"))
 # library(devtools)
 # devtools::install_github("rmcelreath/rethinking")
@@ -42,11 +41,7 @@ library(rethinking)
 library(brms)
 library('readxl')
 
-setwd("/home/mauriciok8/Dropbox/Projects_JM/Muenster/Intro_Bayesian_Stats/")
-
-
-data = read_xls("Examples//Anaya-Rojas et al 2016/Fish_and_TankData.xls")
-
+data = read_xls("Examples/Anaya-Rojas et al 2016/Fish_and_TankData.xls")
 data[1:8,]
 
 data$Origin = factor(data$Origin)
@@ -58,6 +53,7 @@ levels(data$Nutrients)
 data$Exposure = factor(data$Exposure)
 levels(data$Exposure)
 
+# create dummy variables
 data$Lake = ifelse(data$Origin == "Lake", 1, 0)
 data$High = ifelse(data$Nutrients == "high", 1, 0)
 data$G = ifelse(data$Exposure == 'G+', 1, 0)
@@ -65,15 +61,19 @@ data$CI = log(data$HIS)
 data$Gyro = log(data$Gyrocount+1)
 data$Tank = factor(data$Tank)
 levels(data$Tank) = 1:length(levels(data$Tank))
-
 data$Block = factor(data$Block)
 levels(data$Block) = 1:length(levels(data$Block))
 
-m.data = data[complete.cases(data),c("CI", 'Lake','High','G','Tank','Block','Gyro')]
 
+# Keep only the data needed for the anlysis
+m.data = data[,c("CI", 'Lake','High','G','Tank','Block','Gyro')]
+m.data$Tank = factor(m.data$Tank)
+levels(m.data$Tank) = 1:length(levels(m.data$Tank)) # make sure that the levels are correct
+m.data$Block = factor(m.data$Block)
+levels(m.data$Block) = 1:length(levels(m.data$Block))  # make sure that the levels are correct
+str(m.data) # see the structure of the m.data
 
-str(m.data)
-
+# Run a model describing the relationship between body condition (CI) and parasite load (Gyro)
 m.1 <- ulam(
 
     alist(
@@ -82,16 +82,16 @@ m.1 <- ulam(
     alpha ~ dnorm(0,10),              # prior intercept
     beta_G ~ dnorm(0,10),             # prior slope 
     sigma ~ exponential(1)            # prior error
-    ), data = m.data, 
+    ), data = m.data, log_lik = TRUE,
     chains = 4, 
     cores = 4,
     iter = 2000,
     warmup = 1000
 )
 
-precis(m.1, prob=.95)
+traceplot_ulam(m.1) # 
 
-traceplot_ulam(m.1)
+precis(m.1, prob=.95) # look at the model estimations
 
 m.2 <- ulam(
 
@@ -108,7 +108,34 @@ m.2 <- ulam(
     sigma_t ~ exponential(1),
     sigma_b ~ exponential(1)    
         
-    ), data = m.data, 
+    ), data = m.data, log_lik = TRUE,
+    chains = 4, 
+    cores = 4,
+    iter = 4000,
+    warmup = 2000,
+    control = list(adapt_delta = 0.92, max_treedepth = 11) # options to tune up the model
+)
+
+traceplot_ulam(m.2)
+
+m.3 <- ulam(
+
+    alist(
+    CI ~ dnorm(mu, sigma),            # likelihood
+    mu <- alpha + beta_G * Gyro + beta_L*Lake +
+        v_t[Tank]  + v_b[Block],      # linear model
+    alpha ~ dnorm(0,10),              # prior intercept
+    beta_G ~ dnorm(0,10),             # prior slope 
+    beta_L ~ dnorm(0,10),             # prior slope 
+    sigma ~ exponential(1),            # prior error
+  
+# Group level effects
+    v_t[Tank] ~ dnorm(0,sigma_t), 
+    v_b[Block] ~ dnorm(0,sigma_b),
+    sigma_t ~ exponential(1),
+    sigma_b ~ exponential(1)    
+        
+    ), data = m.data, log_lik = TRUE,
     chains = 4, 
     cores = 4,
     iter = 2000,
@@ -116,95 +143,128 @@ m.2 <- ulam(
 )
 
 
+m.4 <- ulam(
+
+    alist(
+    CI ~ dnorm(mu, sigma),            # likelihood
+    mu <- alpha + beta_G * Gyro + beta_L*Lake + beta_GL*(Lake*Gyro) +
+        v_t[Tank] + v_b[Block],      # linear model
+    alpha ~ dnorm(0,10),              # prior intercept
+    beta_G ~ dnorm(0,10),             # prior slope 
+    beta_L ~ dnorm(0,10),             # prior slope 
+    beta_GL ~ dnorm(0,10),             # prior slope 
+    sigma ~ exponential(1),            # prior error
+  
+# Group level effects
+    v_t[Tank] ~ dnorm(0,sigma_t), 
+    v_b[Block] ~ dnorm(0,sigma_b),
+    sigma_t ~ exponential(1),
+    sigma_b ~ exponential(1)    
+        
+    ), data = m.data, log_lik = TRUE,
+    chains = 4, 
+    cores = 4,
+    iter = 2000,
+    warmup = 1000
+)
+
+traceplot_ulam(m.4)
+
+compare(m.1, m.2, m.3, m.4)
+
+post4 = extract.samples(m.4)
+str(post4)
+
+p.link = function(posteriors, Gyros = NULL, Lake = 1){
+   Gyros<- log(Gyros+1)
+   pred<- with(posteriors, alpha + beta_G*Gyros + beta_L*Lake + beta_GL*(Gyros*Lake))
+   return(pred)
+}
+
+# Estimate the body condition of a lake fish with 10 parasites
+mean(p.link(posteriors  = post4, Gyros = 10, Lake = 1))
+HPDI(p.link(posteriors  = post4, Gyros = 10, Lake = 1), prob=.95)
+
+Lgyro_count = seq(from = 0, to = max(m.data$Gyro[which(m.data$Lake == 1)]), length.out = 100)
+raw.L = sapply(1:length(Lgyro_count), function(i)p.link(posteriors  = post4, Gyros = Lgyro_count[i], Lake = 1))
+Sgyro_count = seq(from = 0, to = max(m.data$Gyro[which(m.data$Lake == 0)]), length.out = 100)
+raw.S = sapply(1:length(Sgyro_count), function(i)p.link(posteriors  = post4, Gyros = Sgyro_count[i], Lake = 0))
+
+str(raw.L)
+
+mean.L = apply(raw.L, 2, mean)
+mean.L
+
+ci.L = apply(raw.L, 2, HPDI)
+ci.L
+
+mean.S = apply(raw.S, 2, mean)
+ci.S = apply(raw.S, 2, HPDI)
+
+# does parasite load reduces body condition?
+post2 = extract.samples(m.2)
+dens(post2$beta_G, show.HPDI = TRUE, show.zero = TRUE)
+
+# what is the probability of $\beta_{G} < 0$?
+n_negative = length(which(post2$beta_G <0 ))
+n_total = length(post2$beta_G )
+round((n_negative/n_total) * 100,3) 
+
+# is this relationship different between ecotypes?
+# what is the probability of $\beta_{GL} < 0$?
+n_negative = length(which(post4$beta_GL >0 ))
+n_total = length(post4$beta_GL )
+round((n_negative/n_total) * 100,3) 
 
 
-Condition_1 = brm(Condition ~ Gyro +  (1|Tank/Block),
-              family = gaussian(),
-             data = data, 
-              cores = 2, chains = 2, iter= 3000, warmup=1500
-             )
+plot(CI ~ Gyro, m.data, pch="", xlab = "Gyrodactylus (log)")
+points(CI ~ Gyro, subset(m.data, Lake == 1), pch=21, bg = "dodgerblue2" ,cex = 1.5)
+lines(x =Lgyro_count, y = mean.L, col = "dodgerblue2", lty=1, lwd=3 )
+shade(ci.L, Lgyro_count, col = col.alpha('dodgerblue2', 0.3) )
 
+points(CI ~ Gyro, subset(m.data, Lake == 0), pch=21, bg = "tomato" ,cex = 1.5)
+lines(x =Sgyro_count, y = mean.S, col = "tomato", lty=1, lwd=3 )
+shade(ci.S, Sgyro_count, col = col.alpha('tomato', 0.3) )
 
+precis(m.4, prob=.89)
 
-plot(Condition_1)
-
-# use the summary function to see the bayesian inference
-summary(Condition_1)
-
-conditional_effects(Condition_1, effects = "Gyro")
-
-
-
-# testing interactions
-Condition_2 = brm(Condition ~ Gyro*Origin*Nutrients +  (1|Tank/Block),
-              family = gaussian(),
-             data = data, 
-              cores = 2, chains = 2, iter= 3000, warmup=1500
-             )
-
-
-summary(Condition_2)
-
-conditional_effects(Condition_2, effects = "Gyro:Nutrients")
-
-loo_R2(Condition_1)
-loo_R2(Condition_2)
-
-Condition_1 = add_criterion(Condition_1, 'loo', moment_match = TRUE)
-Condition_2 = add_criterion(Condition_2, 'loo', moment_match = TRUE)
-
-loo_compare(Condition_1, Condition_2)
-
-loo_model_weights(Condition_1, Condition_2)
-
-
+glimmer(CI ~ Gyro + (1|Tank|Block), data = m.data)
 
 Gyro_1 = brm(Gyrocount ~ Origin*Nutrients*Exposure + offset(Sd.lenght.mm) +
               (1|Tank/Block),
               family = zero_inflated_negbinomial,
              data = data, 
-              cores = 2, chains = 2, iter= 3000, warmup=1500
+              cores = 4, chains = 4, iter= 4000, warmup=2000,
+             control = list(adapt_delta = 0.95, max_treedepth = 14)
              )
 
 plot(Gyro_1)
 
-Gyro_2 =brm(Gyrocount ~ Origin*Exposure + offset(Sd.lenght.mm) +
+summary(Gyro_1)
+
+conditional_effects(Gyro_1, effects = "Exposure:Origin")
+
+Gyro_2 = brm(Gyrocount ~ Origin*Exposure + offset(Sd.lenght.mm) +
               (1|Tank/Block),
               family = zero_inflated_negbinomial,
              data = data, 
-              cores = 2, chains = 2, iter= 3000, warmup=1500
+              cores = 4, chains = 4, iter= 4000, warmup=2000,
+             control = list(adapt_delta = 0.95, max_treedepth = 14)
              )
 
-Gyro_3 =brm(Gyrocount ~ Origin*Exposure*Nutrients + offset(Sd.lenght.mm) +
-              (1|Tank/Block),
-              family = zero_inflated_negbinomial,
-             data = data, 
-              cores = 2, chains = 2, iter= 3000, warmup=1500
-             )
+Gyro_1 <- add_criterion(Gyro_1, criterion = 'loo')
+Gyro_2 <- add_criterion(Gyro_2, criterion = 'loo')
+loo_compare(Gyro_1, Gyro_2)
 
-# add criterion to the models for comparison
+loo_model_weights(Gyro_1, Gyro_2)
 
-Gyro_1 = add_criterion(Gyro_1, 'loo')
-Gyro_2 = add_criterion(Gyro_2, 'loo')
-Gyro_3 = add_criterion(Gyro_3, 'loo')
-# compare models
+post_G2 = posterior_samples(Gyro_2)
+str(post_G2)
 
-loo_compare(Gyro_1, Gyro_2, Gyro_3, criterion = 'loo')
+# What is the probability that Lake G+ have more parasites than Stream G+ fish?
+LG = post_G2$b_Intercept + post_G2$b_OriginStream*0 + post_G2$b_ExposureGP*1 + post_G2$`b_OriginStream:ExposureGP`*0
+SG = post_G2$b_Intercept + post_G2$b_OriginStream*1 + post_G2$b_ExposureGP*1 + post_G2$`b_OriginStream:ExposureGP`*1
 
-# estimate model averaging
-loo_model_weights(Gyro_1, Gyro_2, Gyro_3, criterion = 'loo')
-
-summary(Gyro_2)
-
-conditional_effects(Gyro_2, effects = "Origin:Exposure")
-
-# What is the probability that expose lake fish have higher numbers of parasites than no-exposed lake fish?
-post2 = posterior_samples(Gyro_2)
-
-LakeGyr = post2$b_Intercept + post2$b_OriginStream * 0 + post2$b_ExposureYes * 1  + post2$`b_OriginStream:ExposureYes`*0
-LakeNoGyr = post2$b_Intercept + post2$b_OriginStream * 0 + post2$b_ExposureYes * 0  + post2$`b_OriginStream:ExposureYes`*0
-length(which(LakeGyr - LakeNoGyr > 0))/ length(LakeGyr)
-
-# the probability that expose lake fish have higher numbers of parasites than no-exposed lake fish:
-
-
+n_negative = length(which(LG - SG > 0 ))
+n_total = length(LG)
+round((n_negative/n_total) * 100,3) 
--- README.md
+++ README.md
@@ -1,43 +1,58 @@
-# Introduction to Bayesian Statistic for Experimental Ecology
+## Install `R` and the necessary packages
+This is something you have to do only once.
 
-In this repository, you can find a short introduction to Bayesian statistical inference using `Stan` via `R`. If you are familiar with `Jupyter Notebooks` you can follow the examples done in the lecture in the "Introduction Notebook.ipynb" file. Otherwise, you can find the `R code` in the `\R` folder. 
-
-For an introduction to **Jupyter Notebooks** and how to install it togeter with `R` follow instruction in the next link ([Click here](https://www.youtube.com/watch?v=gwRhPojbINI)). Do not create an r-enviroment in Anaconda, follow the instructinos here to make R available to the notebooks
-
-https://www.datacamp.com/community/blog/jupyter-notebook-r
-
-
-The easiest way to use this reoisutiry it is to clone (download) it and run it on your local machine, if you want to clone it via GitHub go ahead, [here](https://www.youtube.com/watch?v=E2d91v1Twcc&t=351s)
- you can find a nice tutorial on how to do it. 
+- to install `R` go to this webpage, download it and follow the instructions [click](https://www.r-project.org/)
+- then, open `R,` copy and paste the following text without the `#.` This process will take some minutes.
 
 # Are you new to R?
-## Don’t worry there is a lot of help out there, you can start with any of the following tutorials:
+## Don't worry, there is a lot of help out there. You can start with any of the following tutorials:
 
 [tutorial 1](https://www.youtube.com/watch?v=fDRa82lxzaU)
 [tutorial 2](https://www.youtube.com/watch?v=_V8eKsto3Ug)
 
-If you want to learn more about Bayesian statistics, I recommend [Richard McElreath ‘s lectures](https://github.com/rmcelreath/statrethinking_winter2019) and [amazing book](https://www.amazon.de/Statistical-Rethinking-Bayesian-Examples-Chapman/dp/036713991X/ref=sr_1_1?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2U80GHSYN6SXD&dchild=1&keywords=statistical+rethinking&qid=1593046024&sprefix=statistical+re%2Caps%2C337&sr=8-1).
+
+## If you want to use the Jupyter Notebooks, install python3 and Anaconda in your computer
+
+Follow the instructions in this page
+
+[https://www.anaconda.com/products/individual](https://www.anaconda.com/products/individual)
+
+- After installing anaconda, you will have Jupyter notebooks installed. Now you need to make `R` available to Jupyter notebooks. To do that follow the instructions in the website bellow:
+https://www.datacamp.com/community/blog/jupyter-notebook-r
+
+You can learn more about Jupyter Notebooks  in this video [click](https://www.youtube.com/watch?v=jZ952vChhuI) 
 
 
-I also recommend to  [Paul Buerkner’s amazing package and his tutorials](https://github.com/paul-buerkner/brms)
+# Installing `R-studio`
+
+The best way to develop R code in your computer is using R-studio, it is a nice interface that help you a lot in the process of writing the code, interacting with R, and so many more things...
+
+[https://rstudio.com/products/rstudio/](https://rstudio.com/products/rstudio/)
+
+
+# Introduction to Bayesian Statistics for Experimental Ecology
+
+You can find a short introduction to Bayesian statistical inference using `Stan` via `R` in this repository. If you are familiar with `Jupyter Notebooks,` you can follow the lecture's examples in the "Introduction Notebook.ipynb" file. Otherwise, you can find the `R code` in the `\R` folder. 
+
+If you want to learn more about Bayesian statistics, I recommend [Richard McElreath's lectures](https://github.com/rmcelreath/statrethinking_winter2019) and [amazing book](https://www.amazon.de/Statistical-Rethinking-Bayesian-Examples-Chapman/dp/036713991X/ref=sr_1_1?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2U80GHSYN6SXD&dchild=1&keywords=statistical+rethinking&qid=1593046024&sprefix=statistical+re%2Caps%2C337&sr=8-1).
+
+
+I also recommend [Paul Buerkner's amazing package and his tutorials](https://github.com/paul-buerkner/brms)
 
 
 ## More about Stan, [click here](https://www.youtube.com/watch?v=4t6niM6sksI)
  
 
-# Assignments
-I have taken the liberty to create a folder for each one of you in the [Assignments folder](https://github.com/JaimeMAnayaRojas/Muenster_EE2020/tree/master/Assigments). Each one of you have to re-run some of the analyses of the papers associated with the data that is provided for each of you. The basics of how to do it have been covered in class, but do not hesitate to contact me. 
-
-Each one of you must also write a small report of want you have found and submit your code to me, to check.
-
 ## some things to keep in mind:
 
-You will be working with published data and they belong to well plan experiments, so be aware of which are the replicates and the experimental block.
-Also, be aware of using the right family distribution, if you want to know when to use them see [Ben Bolker’s paper on GLMMs](https://www.sciencedirect.com/science/article/abs/pii/S0169534709000196). 
+You will be working with published data. They belong to well plan experiments, so be aware of the replicates and the experimental block.
+Also, be aware of using the right family distribution; if you want to know when to use them, see [Ben Bolker's paper on GLMMs](https://www.sciencedirect.com/science/article/abs/pii/S0169534709000196). 
 Gaussian or normal (most linear regressions)
 Poisson and negative binomial (most GLMs with count data)
-Binomial or Bernulli (binomial data, 1 or 0, survival or death, etc)
+Binomial or Bernoulli (binomial data, 1 or 0, survival or death, etc)
 Include random effects to avoid pseudo-replications [link](http://lbs2011.lbsconference.org/cartography.tuwien.ac.at/drupalmultisite/sites/lbs2011.org/files/presentations/54_1.pdf)
 
 
-
+# Using this repository
+The easiest way to use this repository is to clone (download) it and run it on your local machine; if you want to clone it via GitHub go ahead and help this tutorials to improve feel free, [here](https://www.youtube.com/watch?v=E2d91v1Twcc&t=351s)
+ you can find a nice tutorial on how to do it. 
